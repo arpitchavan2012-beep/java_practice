14e5809cd435c8125f416e0864f2aa9dd1454224
arpit chavan <arpitchavan2012@gmail.com>
2026-03-20 20:57:51 +0530

    new_files

---
+++ Ascii.java
@@ -0,0 +1,6 @@
+public class Ascii {
+    public static void main(String[]a){
+    for (int i = 0; i<127; i++){
+        System.out.printf("%d : %c\n",i,i);
+    }}
+}
---
+++ DemoLoops.java
@@ -0,0 +1,19 @@
+public class DemoLoops {
+    
+    public static void main(String [] a){
+
+        int i = 1;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+        int j = 28;
+        do{
+            System.out.println(j);
+            i++;
+        }while(j<=10);
+        for (int k = 0; k<=5; k++){
+            System.out.println(k);
+        }
+    }
+}
