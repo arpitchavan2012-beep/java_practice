842dfdc4c29733d9ca2f0bdc38525a15bc89d3b4
arpit chavan <arpitchavan2012@gmail.com>
2026-03-26 11:19:10 +0530

    files

--- Star.java
+++ Star.java
@@ -38,5 +38,12 @@ public class Star {
                     System.out.print("*");
                 }
                 System.out.println();}
+                System.out.println();
+                for(int i = 0; i <=4;i++){
+                    for(int j = 1 ; j<=4-i;j++){
+                        System.out.print(" ");
+                }                for (int j = 1;j<=2 * i-1;j++){
+                    System.out.print("*");
+                }
     }
 }
